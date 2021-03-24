a1d8eaff2ee56f143b9abefafda97be2b3fe1a43
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2021-03-24 13:00:30 -0300

    questão 1 pronta

--- dwt.py
+++ dwt.py
@@ -9,15 +9,15 @@ returna
   - Detalhes D
 """
 def single_dwt_decomposition(signal, c, d):
-  y1, y2 = [], []
+  y0, y1 = [], []
   S = [0] + signal + [0]
   for i in range(len(S)-1): # convolução
-    y1.append( S[i] * c[0] + S[i+1] * c[1] )
-    y2.append( S[i] * d[0] + S[i+1] * d[1] )
+    y0.append( S[i] * c[0] + S[i+1] * c[1] )
+    y1.append( S[i] * d[0] + S[i+1] * d[1] )
   A, D = [], []
   for i in range(1,len(S)-1,2): # down-sampling
-    A.append( y1[i] )
-    D.append( y2[i] )
+    A.append( y0[i] )
+    D.append( y1[i] )
   return (A, D)
 
 """
@@ -31,13 +31,11 @@ returna
 """
 def single_dwt_reconstruction(signal, detail, f, g):
   I = []
-  A, D = [], []
-  for i in range(2*len(signal)-1): # up-sampling
-    A.append( 0 if i%2 == 1 else signal.pop(0) )
-    D.append( 0 if i%2 == 1 else detail.pop(0) )
-  A = A + [0]
-  D = D + [0]
-  for i in range(len(A)-1): # reconstrução
+  A, D = [0], [0]
+  for i in range(len(detail)): # up-sampling
+    A.extend( [signal.pop(0), 0] )
+    D.extend( [detail.pop(0), 0] )
+  for i in range(len(D)-1): # reconstrução
     a = A[i] * f[0] + A[i+1] * f[1]
     d = D[i] * g[0] + D[i+1] * g[1]
     I.append(a + d)
@@ -57,8 +55,8 @@ def dwt1D(signal, j, c, d):
   Ds = []
   A = signal
   while j > 0:
-    A, d = single_dwt_decomposition(A, c, d)
-    Ds.insert(0, d)
+    A, dt = single_dwt_decomposition(A, c, d)
+    Ds.append(dt)
     j -= 1
   return (A, Ds)
 
@@ -72,11 +70,10 @@ função DWT de reconstrução em J níveis
 returna 
   - Sinal Original
 """
-def idwt1D(signal, details, j, f, g):
+def idwt1D(signal, details, f, g):
   Ds = []
   A = signal
-  while j > 0 or len(details) > 0:
-    d = details.pop(0)
+  while len(details) > 0:
+    d = details.pop()
     A = single_dwt_reconstruction(A, d, f, g)
-    j -= 0
   return A
--- questao1.py
+++ questao1.py
@@ -6,23 +6,31 @@ from dwt import single_dwt_decomposition, single_dwt_reconstruction, dwt1D, idwt
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Processa um sinal 1D com a DWT')
-  parser.add_argument('signal', metavar='S', type=int, nargs='+', default=[1, 2, -1, 3, 6, -2, -1, 3], help='Sinal 1D a ser processado')
+  parser.add_argument('signal', metavar='S', type=float, nargs='+', default=[-0.7071067811865475, 3.5355339059327373, 0.7071067811865475], help='Sinal 1D a ser processado')
   parser.add_argument('-J', type=int, default=1, help='Nível máximo de decomposição da DWT')
   args = parser.parse_args()
 
   signal = args.signal
   J = args.J
 
-  print('Sinal Original\t', signal)
+  print('Sinal Original\n\t', signal)
   
   haar_c = [1/sqrt(2), 1/sqrt(2)]
   haar_d = [-1/sqrt(2), 1/sqrt(2)]
   haar_f = [1/sqrt(2), 1/sqrt(2)]
   haar_g = [1/sqrt(2), -1/sqrt(2)]
 
-  filtrado, detalhes = single_dwt_decomposition(signal, haar_c, haar_d)
-  print('Filtro Passa-Baixa\t', filtrado)
-  print('Filtro Passa-Alta\t', detalhes)
+  filtrado, detalhes = dwt1D(signal, J, haar_c, haar_d)
+  formatted = ['{:.5}' for i in filtrado]
+  str_formatted = ', '.join(formatted)
+  print('Filtrado\n\t', f"[ {str_formatted.format(*filtrado)} ]")
+  print('Detalhes')
+  for ix,d in enumerate(detalhes):
+    formatted = ['{:.5}' for i in d]
+    str_formatted = ', '.join(formatted)
+    print(f"#{ix}\t", f"[ {str_formatted.format(*d)} ]")
 
-  reconstruido = single_dwt_reconstruction(filtrado, detalhes, haar_f, haar_g)
-  print('Sinal Reconstruído\t', reconstruido)
+  reconstruido = idwt1D(filtrado, detalhes, haar_f, haar_g)
+  formatted = ['{:.5}' for i in reconstruido]
+  str_formatted = ', '.join(formatted)
+  print('Sinal Reconstruído\n\t', f"[ {str_formatted.format(*reconstruido)} ]")
